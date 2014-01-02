216c284b94870a244c98edbe87c0b4d0ece5f6fa
sdshrest@gmail.com <sdshrest@gmail.com>
2014-01-01 22:39:36 -0600

    added contributions table + angularjs sort

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -19,35 +19,6 @@ parishionerApp.controller( "grabContributions", function( $scope, $http ) {
     $scope.contributions = {};
     $http.post( 'data/contributions.json' ).success( function ( data ) {
         $scope.contributions = data.posted;
-        // var log = [];
-        // angular.forEach( contributions, function( value, key ) {
-        //     this.push( key + ': ' + value );
-        // }, log );
+        $scope.predicate = ['date_given', "fund_name"];
     });
 })
-
-// parishionerApp.controller( "grabData", function( $scope, $http ) {
-//     $scope.index = 0;
-//     $scope.households = {};
-//     $http.JsonP( 'https://secure3.iconcmo.com/api/', {
-//         "Auth": {
-//             "Phone": "5555555555",
-//             "Username": "JoePUser",
-//             "Password": "password"
-//         },
-//         "Request": {
-//             "Module": "membership",
-//             "Section": "directory",
-//             "Filters": {
-//                 "id": 721
-//             },
-//             "Sort": {
-//                 "last_name": "ascending",
-//                 "first_name": "ascending"
-//             }
-//         }
-//     } ).success( function ( data ) {
-//         $scope.households = data.directory;
-// //         console.log( $scope.households[0] );
-//     });
-// });
